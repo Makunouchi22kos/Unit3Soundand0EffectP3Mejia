d08a940d2a9cb8ac08bbc47cead1fb7a288e2e4a
Allan Mejia-Mora <723495@student.dusd.net>
2024-04-10 11:38:49 -0700

    Moveleft

--- Unit3Soundand0EffectP3Mejia/Assets/Scripts/MoveLeft.cs
+++ Unit3Soundand0EffectP3Mejia/Assets/Scripts/MoveLeft.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
-{w
+{
     private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
